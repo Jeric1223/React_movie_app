af75a48c43d07af6083013304c59f731db4feffc
JaeHyun <soehd0889@naver.com>
2021-01-26 23:05:30 +0900

    [2021-01-26] state 활용

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,66 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const foodILike = [
-    {
-        id: 1,
-        name: 'Kimchi',
-        image:
-            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.B4QMCfh6O7edtNrJUUs4mQHaE8%26pid%3DApi&f=1',
-        rating: 5,
-    },
-    {
-        id: 2,
-        name: 'Bibimbap',
-        image:
-            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F44%2FDolsot-bibimbap.jpg%2F1200px-Dolsot-bibimbap.jpg&f=1&nofb=1',
-        rating: 4.9,
-    },
-    {
-        id: 3,
-        name: 'Cake',
-        image:
-            'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.seriouseats.com%2Fimages%2F2014%2F11%2FBakedOccasions_BirthdayCake.jpg&f=1&nofb=1',
-        rating: 4.6,
-    },
-    {
-        id: 4,
-        name: 'Kimbap',
-        image:
-            'https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg',
-        rating: 3.7,
-    },
-];
-
-function Food({ name, pictrue, rating }) {
-    return (
-        <div>
-            <h2>I Like {name}!</h2>
-            <h4>{rating}/5.0</h4>
-            <img src={pictrue} alt={name} />
-        </div>
-    );
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    pictrue: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-};
-
-function renderFood(dish) {
-    return (
-        <Food
-            key={dish.id}
-            name={dish.name}
-            pictrue={dish.image}
-            rating={dish.rating}
-        />
-    );
-}
-
-function App() {
-    return <div>{foodILike.map(renderFood)}</div>;
+class App extends React.Component {
+    state = {
+        count: 0,
+    };
+    add = () => {
+        this.setState((current) => ({ count: current.count + 1 }));
+    };
+    minus = () => {
+        this.setState((current) => ({ count: current.count - 1 }));
+    };
+    render() {
+        return (
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
+    }
 }
 
 export default App;
